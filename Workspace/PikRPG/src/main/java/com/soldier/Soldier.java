1ea34e48099ae304ca9f738346e73c0086a647b4
Fred-Doell <fjdoell@gmail.com>
2022-06-08 17:11:39 -0400

    added project start

---
+++ Workspace/PikRPG/src/main/java/com/soldier/Soldier.java
@@ -0,0 +1,13 @@
+package com.soldier;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Soldier {
+	int attack;
+	int carryPow;
+}
---
+++ Workspace/PikRPG/src/main/java/com/soldier/SoldierHandler.java
@@ -0,0 +1,5 @@
+package com.soldier;
+
+public class SoldierHandler {
+
+}
